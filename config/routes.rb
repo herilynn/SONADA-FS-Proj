a75a7beafe7d9d665dd869a5c796724a85b4c530
herilynn <4henrylin17@gmail.com>
2023-05-29 16:36:27 -0400

    Added API/users/Show.json.jbuilder?

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   namespace :api, defaults: {format: :json} do
-    # ...
     resources :users, only: [:create]
     resource :session, only: [:create, :show, :destroy]
   end
